5b002041fce48a9bb1b50222d349ec5b006e1e31
Matten-Matten <32074970+Matten-Matten@users.noreply.github.com>
2017-11-25 17:16:30 +0100

    Update enigma2.js

--- enigma2.js
+++ enigma2.js
@@ -47,84 +47,17 @@ var utils =    require(__dirname + '/lib/utils'); // Get common adapter utils
 // adapter will be restarted automatically every time as the configuration changed, e.g system.adapter.example.0
 var adapter = utils.adapter('enigma2');
 
-// is called when adapter shuts down - callback has to be called under any circumstances!
-//adapter.on('unload', function (callback) {
-//    try {
-//        adapter.log.info('cleaned everything up...');
-//        callback();
-//    } catch (e) {
-//        callback();
-//    }
-//});
-
-  // is called if a subscribed object changes
-//adapter.on('objectChange', function (id, obj) {
-//    // Warning, obj can be null if it was deleted
-//    adapter.log.info('objectChange ' + id + ' ' + JSON.stringify(obj));
-//});
-
-// is called if a subscribed state changes
-//adapter.on('stateChange', function (id, state) {
-    // Warning, state can be null if it was deleted
-//    adapter.log.debug('stateChange ' + id + ' ' + JSON.stringify(state));
-
-
-    // you can use the ack flag to detect if it is status (true) or command (false)
-//    if (state && !state.ack) {
-//        adapter.log.debug('ack is not set!');
-//        //OK unack Value, lets Check if its for us:
-//        if(id == "enigma2.0.enigma2.COMMAND") {
-//            adapter.log.debug("Its our Command: " + state.val);
-//            //ACK
-//            adapter.setState(id, {val: state.val, ack: true});
-//            processCommand(state);
-//        }
-//    }
-//});
-
-
-
-// Some message was sent to adapter instance over message box. Used by email, pushover, text2speech, ...
-//adapter.on('message', function (obj) {
-//    if (typeof obj == 'object' && obj.message) {
-//        if (obj.command == 'send') {
-//            // e.g. send email or pushover or whatever
-//            console.log('send command');
-//
-//            // Send response in callback if required
-//            if (obj.callback) adapter.sendTo(obj.from, obj.command, 'Message received', obj.callback);
-//        }
-//    }
-//});
-
-// is called when databases are connected and adapter received configuration.
-// start here!
 adapter.on('ready', function () {
     main();
 });
 
-//frage stellen test  ############################################################################################
-//var messagetext = "test";
-//var messagetyp = "0";
-//var messagetime = "30";
-
-//function processCommand(state) {
-//    if (state.val == "SEND_MESSAGE") {
-//        adapter.log.info("Sending command: " + state.val);
-  //        getResponse (state.val, 1, "/web/message?text=Das%20ist%20ein%20Test&type=0&timeout=30", evaluateCommandResponse);
-//        getResponse (state.val, 1, "/web/message?text="+messagetext+"&type="+messagetyp+"&timeout="+messagetime+"", evaluateCommandResponse);
-//    }
-//}
-//##################################################################################################################
 
 function getResponse (command, deviceId, path, callback){
    // var device = dreamSettings.boxes[deviceId];
     var options = {
         host: adapter.config.IPAddress,
         port: adapter.config.Port,
-		secondharddisk: adapter.config.secondharddisk,
-		BOXWLAN: adapter.config.BOXWLAN,
-		webif: adapter.config.webif,
+        internalharddisk: adapter.config.internalharddisk,
         path: path,
         method: 'GET'
     };
@@ -219,11 +152,8 @@ function evaluateCommandResponse (command, deviceId, xml) {
         case "GETVOLUME":
             adapter.log.debug("Box Volume:" + parseInt(xml.e2volume.e2current[0]));
             adapter.setState('enigma2.VOLUME', {val: parseInt(xml.e2volume.e2current[0]), ack: true});
-		if (adapter.config.webif === 'false' || adapter.config.webif === false){
             adapter.log.debug("Box Muted:" + parseBool(xml.e2volume.e2ismuted));
             adapter.setState('enigma2.MUTED', {val: parseBool(xml.e2volume.e2ismuted), ack: true});
-			};			
-
 			break;
         case "GETCURRENT":
             adapter.log.debug("Box EVENTDURATION:" + parseInt(xml.e2currentserviceinformation.e2eventlist[0].e2event[0].e2eventduration[0]));
@@ -238,10 +168,10 @@ function evaluateCommandResponse (command, deviceId, xml) {
             adapter.setState('enigma2.EVENTDESCRIPTION', {val: xml.e2currentserviceinformation.e2eventlist[0].e2event[0].e2eventdescription[0], ack: true});
 			adapter.log.debug("Box Sender Servicereference: " +xml.e2currentserviceinformation.e2eventlist[0].e2event[0].e2eventservicereference[0]);			
             adapter.setState('enigma2.CHANNEL_SERVICEREFERENCE', {val: xml.e2currentserviceinformation.e2eventlist[0].e2event[0].e2eventservicereference[0], ack: true});
-		if (adapter.config.webif === 'true' || adapter.config.webif === true){
-			adapter.setState('enigma2.MUTED', {val: parseBool(xml.e2currentserviceinformation.e2volume[0].e2ismuted[0]), ack: true});
-			adapter.log.debug("Box Muted:" + parseBool(xml.e2currentserviceinformation.e2volume[0].e2ismuted[0]));
-			};
+
+//			adapter.setState('enigma2.MUTED', {val: parseBool(xml.e2currentserviceinformation.e2volume[0].e2ismuted), ack: true});
+//			adapter.log.debug("Box Muted:" + parseBool(xml.e2currentserviceinformation.e2volume[0].e2ismuted));
+
 		
             break;
         case "GETINFO":
@@ -249,35 +179,17 @@ function evaluateCommandResponse (command, deviceId, xml) {
             adapter.setState('enigma2.CHANNEL', {val: xml.e2abouts.e2about[0].e2servicename[0], ack: true});
 			adapter.log.debug("Box Model: " +xml.e2abouts.e2about[0].e2model[0]);
 			adapter.setState('enigma2.MODEL', {val: xml.e2abouts.e2about[0].e2model[0], ack: true});
-		if (adapter.config.secondharddisk === 'false' || adapter.config.secondharddisk === false){			
+		if (adapter.config.internalharddisk === 'true' || adapter.config.internalharddisk === true){
             adapter.log.debug("Box HDD capacity: " + xml.e2abouts.e2about[0].e2hddinfo[0].capacity[0]);
             adapter.log.debug("Box HDD free: " +xml.e2abouts.e2about[0].e2hddinfo[0].free[0]);
             adapter.setState('enigma2.HDD_CAPACITY', {val: xml.e2abouts.e2about[0].e2hddinfo[0].capacity[0], ack: true});
             adapter.setState('enigma2.HDD_FREE', {val: xml.e2abouts.e2about[0].e2hddinfo[0].free[0], ack: true});	
 			};
-		if (adapter.config.secondharddisk === 'true' || adapter.config.secondharddisk === true){			
-            adapter.log.debug("Box HDD2 capacity: " + xml.e2abouts.e2about[0].e2hddinfo[0].capacity[0]);
-            adapter.log.debug("Box HDD2 free: " +xml.e2abouts.e2about[0].e2hddinfo[0].free[0]);
-            adapter.setState('enigma2.HDD2_CAPACITY', {val: xml.e2abouts.e2about[0].e2hddinfo[0].capacity[0], ack: true});
-            adapter.setState('enigma2.HDD2_FREE', {val: xml.e2abouts.e2about[0].e2hddinfo[0].free[0], ack: true});	
-			};
 			
             break;
         case "DEVICEINFO":
-		if (adapter.config.BOXWLAN === 'true' || adapter.config.secondharddisk === true){
-			adapter.log.debug("Box IP: " +xml.e2deviceinfo.e2network[0].e2interface[1].e2ip[0]);
-            adapter.setState('enigma2.BOX_IP', {val: xml.e2deviceinfo.e2network[0].e2interface[1].e2ip[0], ack: true});
-			};
-		if (adapter.config.BOXWLAN === 'false' || adapter.config.secondharddisk === false){
 			adapter.log.debug("Box IP: " +xml.e2deviceinfo.e2network[0].e2interface[0].e2ip[0]);
             adapter.setState('enigma2.BOX_IP', {val: xml.e2deviceinfo.e2network[0].e2interface[0].e2ip[0], ack: true});
-			};
-		if (adapter.config.secondharddisk === 'true' || adapter.config.secondharddisk === true){
-            adapter.log.debug("Box HDD capacity: " + xml.e2deviceinfo.e2hdds[0].e2hdd[1].e2capacity[0]);	
-	        adapter.log.debug("Box HDD free: " +xml.e2deviceinfo.e2hdds[0].e2hdd[1].e2free[0]);
-            adapter.setState('enigma2.HDD_CAPACITY', {val: xml.e2deviceinfo.e2hdds[0].e2hdd[1].e2capacity[0], ack: true});
-	        adapter.setState('enigma2.HDD_FREE', {val: xml.e2deviceinfo.e2hdds[0].e2hdd[1].e2free[0], ack: true});
-			};
 		
             break;
         case "KEY":
@@ -312,7 +224,7 @@ function checkStatus() {
 			getResponse ("GETCURRENT", 1, "/web/getcurrent", evaluateCommandResponse);
 			getResponse ("DEVICEINFO", 1, "/web/deviceinfo", evaluateCommandResponse);
         } else {
-            adapter.log.debug("enigma2: " + adapter.config.IPAddress + " is not reachable!");
+            adapter.log.debug("enigma2: " + adapter.config.IPAddress + " ist nicht erreichbar!");
         }
     });
 }
@@ -427,6 +339,7 @@ function main() {
         },
         native: {}
     });
+if (adapter.config.internalharddisk === 'true' || adapter.config.internalharddisk === true){
     adapter.setObject('enigma2.HDD_CAPACITY', {
         type: 'state',
         common: {
@@ -435,24 +348,6 @@ function main() {
         },
         native: {}
     });
-	if (adapter.config.secondharddisk === 'true' || adapter.config.secondharddisk === true){
-    adapter.setObject('enigma2.HDD2_CAPACITY', {
-        type: 'state',
-        common: {
-            type: 'string',
-            role: 'state'
-        },
-        native: {}
-    });
-    adapter.setObject('enigma2.HDD2_FREE', {
-        type: 'state',
-        common: {
-            type: 'string',
-            role: 'state'
-        },
-        native: {}
-    });
-	};
     adapter.setObject('enigma2.HDD_FREE', {
         type: 'state',
         common: {
@@ -461,6 +356,7 @@ function main() {
         },
         native: {}
     });
+	};
 	    adapter.setObject('enigma2.MODEL', {
         type: 'state',
         common: {
@@ -477,23 +373,6 @@ function main() {
         },
         native: {}
     });
-
-//    adapter.setObject('enigma2.COMMAND', {
-//        type: 'state',
-//        common: {
-//            type: 'string',
-//            role: 'state'
-//        },
-//        native: {}
-//    });
-//    adapter.setObject('enigma2.MESSAGE_TEXT', {
-//        type: 'state',
-//        common: {
-//            type: 'string',
-//            role: 'state'
-//        },
-//        native: {}
-//    });
 
     // in this example all states changes inside the adapters namespace are subscribed
     adapter.subscribeStates('*');
@@ -536,3 +415,4 @@ function main() {
     adapter.log.info("starting Polling every " + adapter.config.PollingInterval + " ms");
     setInterval(checkStatus,adapter.config.PollingInterval);
 }
+
